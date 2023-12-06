6f176b512f9a97cdd045cb13d0b3752ff3694ad7
Frank <franklinsales1711@gmail.com>
2023-12-05 23:56:15 -0300

    alterei o endereço dos testes para o diretório lib

--- src/main.c
+++ src/main.c
@@ -3,7 +3,7 @@
 
 int main() {
     size_t  size;
-    int **  matrix = ReadMatrixFile( "C:\\Users\\lucas\\Desktop\\tsp3_1194.txt", &size );
+    int **  matrix = ReadMatrixFile( "lib/tsp1_253.txt", &size );
 
     for ( size_t i = 0; i < size; i++ ) {
         for ( size_t j = 0; j < size; j++ ) {
